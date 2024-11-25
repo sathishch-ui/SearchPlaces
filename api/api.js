05ad081d270d197edb2ae49ade1c790e2eebca02
sathishchaviti <sathish.chaviti@goaugment.io>
2024-11-25 21:01:07 +0530

    Updated Constant.js

--- api/api.js
+++ api/api.js
@@ -1,5 +1,4 @@
-const API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
-const API_KEY = "4b55a1c75dmshfa5515160eaa071p1ee945jsnba278b6d4605";
+import { API_KEY, API_URL } from "../utils/constant.js";
 
 export const fetchCities = async (query, page, limit = 5) => {
   const offset = (page - 1) * limit;
---
+++ utils/constant.js
@@ -0,0 +1,2 @@
+export const API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
+export const API_KEY = "4b55a1c75dmshfa5515160eaa071p1ee945jsnba278b6d4605";
